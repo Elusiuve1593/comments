ad702c5dafba1cc87280090c87b64b7eeba02417
Roma <elusiuve15.09.93@icloud.com>
2025-02-07 15:11:48 +0200

    fix

--- src/modules/cloudinary/cloudinary.controller.ts
+++ src/modules/cloudinary/cloudinary.controller.ts
@@ -2,19 +2,16 @@ import {
   Controller,
   Post,
   UploadedFile,
-  UseGuards,
-  UseInterceptors,
+  UseInterceptors
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
 import { multerOptions } from './multer-options';
-import { JwtAuthGuard } from '../user/jwt-auth.guard';
 
 @Controller('image')
 export class CloudinaryController {
   constructor(private readonly cloudinaryService: CloudinaryService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', multerOptions))
   uploadImage(
